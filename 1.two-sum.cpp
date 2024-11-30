0fa04118ad03bb046b5b8bf634af5dad1449eba1
Cateds <this.id.was.used@outlook.com>
2024-11-30 18:39:40 +0800

    2024-11-30 简单题，好

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -28,7 +28,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map<int,int> mp;
+        for (int i=0; i<nums.size(); i++) {
+            if (mp[nums[i]]) {
+                return vector<int> {mp[nums[i]]-1,i};
+            }
+            else mp[target-nums[i]] = i+1;
+        }
+        return vector<int>();
     }
 };
 // @lc code=end
