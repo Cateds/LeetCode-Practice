74262b360ed0da126238593d8e219e9cb682fb67
Cateds <this.id.was.used@outlook.com>
2025-02-04 02:43:03 +0800

    2025-2-4 Format Correction

--- 125.valid-palindrome.cpp
+++ 125.valid-palindrome.cpp
@@ -56,22 +56,7 @@
  */
 
 // @lcpr-template-start
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <climits>
-#include <deque>
-#include <functional>
-#include <iostream>
-#include <list>
-#include <queue>
-#include <stack>
 #include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
 using namespace std;
 // @lcpr-template-end
 // @lc code=start
